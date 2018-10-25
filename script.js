064377e64e4b8a493cd3eff556883a89da7a9c73
zlajabekrija <zlaja.bekrija@gmail.com>
2018-10-25 23:46:57 +0200

    functions added

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function blocks() {
 		square.classList.add('paint' + i);
 		square.setAttribute('onmouseover', 'color(this)');
 		square.style.backgroundColor = 'white';
-		square.style.margin = 'auto';
+		//square.style.margin = 'auto';
 		square.style.height = '100%';
 		square.style.width = '100%';
 		square.style.border = '.1px solid black';
@@ -41,4 +41,47 @@ function blocks() {
 	frame.style.gridTemplateColumns = columns;
 
 
+}
+
+function white() {
+	//resets the grid bringing everything to white
+	//variable bricks is taking every item from grid an reseting it to white
+	let bricks = document.querySelectorAll("[class^=paint]");
+	for (let n = 0; n < bricks.length; n++) {
+		bricks[n].style.backgroundColor = 'white';
+	}
+
+
+}
+
+function black() {
+	let getDivs = document.querySelectorAll("[class^=paint]");
+	for (let m = 0; m < getDivs.length; m++) {
+		getDivs[m].setAttribute('onmouseover', 'mono(this)');
+	}
+
+}
+
+function mono(f) {
+	f.style.backgroundColor = 'black';
+}
+
+function rainbow() {
+	let divs = document.querySelectorAll("[class^=paint]");
+	for (let z = 0; z < divs.length; z++) {
+		divs[z].setAttribute('onmouseover', 'color(this)');
+	}
+}
+
+function erase() {
+	
+	let setDivs = document.querySelectorAll("[class^=paint]");
+	
+	for (let a = 0; a < setDivs.length; a++) {
+		setDivs[a].setAttribute('onmouseover', 'simple(this)');
+	}
+}
+
+function simple(b) {
+	b.style.backgroundColor = 'white';
 }
